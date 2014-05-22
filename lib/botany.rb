40734a84a6a87053c09ac92c627df060ee22a79b
Chris Hoffman <choffman@optoro.com>
2014-05-22 18:38:07 -0400

    Made Botany use Classifier instances instead of classes.
    This is a far more idiomatically Ruby way to go.

--- lib/botany.rb
+++ lib/botany.rb
@@ -2,13 +2,19 @@ require 'botany/version'
 
 module Botany
   class Classifier
+    def initialize &blk
+      instance_eval(&blk) if block_given?
+    end
+
     def self.checks_with checks_module
       subject_class.send :include, checks_module
     end
+    def checks_with checks_module; self.class.checks_with checks_module; end
 
     def self.check name, &definition
       subject_class.send :define_method, name, &definition
     end
+    def check name, &definition; self.class.check name, &definition; end
 
     def self.subject_class
       if const_defined? :RuleSubject
@@ -17,77 +23,74 @@ module Botany
         const_set :RuleSubject, Class.new(Botany::Subject)
       end
     end
+    def subject_class; self.class.subject_class; end
 
-    def self.classifies classification
+    def classifies classification
       (rules << Botany::Rule.new(classification)).last
     end
 
-    def self.default_to classification
+    def default_to classification
       rules << Botany::DefaultRule.new(classification)
     end
+    alias defaults_to default_to
 
-    def self.defaults_to classification
-      default_to classification
-    end
-
-    def self.method_missing sym, *args, &blk
+    def method_missing sym, *args, &blk
       is_check?(sym) ? CheckSet.new(sym, *args) : super
     end
 
-    def self.rules
+    def respond_to_missing? sym, include_private_methods = false
+      is_check?(sym) || super
+    end
+
+    def rules
       @rules ||= []
     end
 
-    def self.is_check? sym
+    def is_check? sym
       @check_tester ||= subject_class.new({})
 
       @check_tester.respond_to? sym
     end
 
-    def self.classify hash
+    def classify hash
       subject = subject_class.new hash
 
       if rule = rule_for_subject(subject)
         rule.classification
       else
-        raise NoApplicableRuleException
+        raise NoApplicableRule
       end
     end
 
-    def self.rule_for_subject subject
+    def rule_for_subject subject
       rules.find do |rule|
         rule.applies_to_subject? subject
       end
     end
   end
 
-  NoApplicableRuleException = Class.new StandardError
-
-  class Rule
-    def initialize classification
-      @classification = classification
-    end
-
-    attr_reader :check_set, :classification
-
-    def when check_set
-      @check_set = check_set
-    end
-
-    def applies_to_subject? subject
-      @check_set ? @check_set.applies_to_subject?(subject) : true
-    end
+  class NoApplicableRule < StandardError
   end
 
   class DefaultRule
     def initialize classification
       @classification = classification
     end
-
     attr_reader :classification
 
     def applies_to_subject? subject
       true
+    end
+  end
+
+  class Rule < DefaultRule
+    def when check_set
+      @check_set = check_set
+    end
+    attr_reader :check_set
+
+    def applies_to_subject? subject
+      check_set ? check_set.applies_to_subject?(subject) : super
     end
   end
 
@@ -96,7 +99,6 @@ module Botany
       @checks = [Check.new(*args)]
       @invert_applicability = false
     end
-
     attr_reader :checks, :invert_applicability
 
     def applies_to_subject? subject
@@ -114,7 +116,7 @@ module Botany
     end
 
     def & check_set
-      @checks.concat check_set.checks
+      checks.concat check_set.checks
       self
     end
 
@@ -128,7 +130,6 @@ module Botany
     def initialize *args
       @proc = Proc.new { send *args }
     end
-
     attr_reader :proc
 
     def true_for_subject? subject
